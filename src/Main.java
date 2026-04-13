9928f0f2ccfc7ec1cb275efad0bdbc137423e8e9
UDin9990 <dineswarenraj@1utar.my>
2026-04-13 21:31:25 +0800

    Dinesh: added startup message in Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Student Management System - Starting...");
         AppGUI window = new AppGUI();
         window.pack();
         window.setVisible(true);
