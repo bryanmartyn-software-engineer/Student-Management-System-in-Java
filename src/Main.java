026d57f6e70694389d7165fcf1f79c608fe16e89
AimaanAzhar <aimaanazhar26@hotmail.com>
2026-04-14 17:11:30 +0800

    Added welcome message when starting the app

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 import javax.swing.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String currentTime = dtf.format(LocalDateTime.now());
+
+        JOptionPane.showMessageDialog(null,
+                "Welcome to the Student Management System\n Session started: " + currentTime,
+                "System Initialisation", JOptionPane.INFORMATION_MESSAGE);
+
         System.out.println("Student Management System - Starting...");
         AppGUI window = new AppGUI();
         window.pack();
@@ -9,3 +18,4 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
+
